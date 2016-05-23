d826dde65d6af3e2f849ecfce72d792d8025ed03
Peter <tcc1011pm@yahoo.com>
2016-05-23 23:16:19 +0100

    add index blade

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -7,11 +7,14 @@
     @foreach ($articles as $article)
         <article>
             <h2>
-                {{ $article->title }}
-
-                <div class="body">{{ $article->body }}</div>        
-
+                <!-- <a href="articles/{{ $article->id }}">{{ $article->title }}</ -->
+                <a href="{{action('ArticlesController@show', [$article->id]) }}">{{ $article->title }}</a>
             </h2>
+          
+
+            <div class="body">{{ $article->body }}</div>        
+
+            
         </article>
     @endforeach
 
