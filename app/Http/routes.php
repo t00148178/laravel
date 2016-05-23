8d6aa48e9c4862f30ac1dd3986581ef6ecd6f5a2
Peter <tcc1011pm@yahoo.com>
2016-05-23 23:13:53 +0100

    me

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,8 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', 'PagesController@about');
+Route::get('about', 'PagesController@about');
 
-Route::get('/contact', 'PagesController@contact');
+Route::get('contact', 'PagesController@contact');
 
-Route::get('/articles', 'ArticlesController@index');
+Route::get('articles', 'ArticlesController@index');
+Route::get('articles/{id}', 'ArticlesController@show');
