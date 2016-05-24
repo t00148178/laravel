e1da91eab68986242a1595dd2eeda573de4dcb7b
Peter <tcc1011pm@yahoo.com>
2016-05-24 20:38:44 +0100

    edit

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,38 @@
+@extends ('app')
+
+@section('content')
+    
+    <h1>Write a new Article</h1>
+	
+	<hr>
+
+	{!! Form::open(array('url' => 'articles')) !!}
+		<div class="form-group">
+			{!! Form::label('title', 'Title')!!}
+			{!! Form::text('title', null, ['class' => 'form-control'])!!}
+		</div>
+
+		<div class="form-group">
+			{!! Form::label('body', 'Body')!!}
+			{!! Form::textarea('body', null, ['class' => 'form-control'])!!}
+		</div>		
+
+
+		<div class="form-group">
+			{!! Form::label('Published_at', 'Published on')!!}
+			{!! Form::input('date', 'published_at', null, ['class' => 'form-control'])!!}
+		</div>	
+
+		<div class="form-group">
+			{!! Form::submit('Add Article', ['class' => 'btn btn-primary form-control'])!!}
+		
+		</div>
+
+
+
+	{!! Form::close() !!}
+
+
+	
+
+@stop    
